075c1492a234c18c386a03ab082b493745498e0e
Imanalopher <iman.mamadiyev@gmail.com>
2016-10-19 12:46:03 +0600

    add key and secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  # provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :github, '67d73c22a839dba9b858', 'a05a846a703a25a4d0330c210e3075961c2bc588'
 end
