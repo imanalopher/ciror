e5c63cdbd2bd404cc995178f2964f5ed9ab1aff5
Imanalopher <iman.mamadiyev@gmail.com>
2016-10-19 19:55:21 +0600

    showing users in index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root "/" => "users#index"
   root :to => "users#index"
   get '/auth/:provider/callback', to: 'sessions#create'
+  get  '/sign_out',  to: 'sessions#destroy'
 end
