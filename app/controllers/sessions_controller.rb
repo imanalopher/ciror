8806654fd96f8dea249c589ab13d441f183c1ba7
Imanalopher <iman.mamadiyev@gmail.com>
2016-10-19 12:15:31 +0600

    show result on yaml format just fot test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
-    session[:user_id] = user.id
+    # user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    # session[:user_id] = user.id
+    raise request.env["omniauth.auth"].to_yaml
     redirect_to root_url, :notice => "Signed in!"
   end
 
